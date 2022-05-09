7aa275366ccb0a380e0b5b80b22b3c9b9d579731
AsierBreyas <javiasterio@gmail.com>
2022-05-09 09:12:45 +0200

    Registro creado

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../api-service/users.service';
+import { User } from '../Models/User';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User;
+  email: FormControl;
+  password: FormControl;
+  name: FormControl;
+  constructor(private userService: UsersService, private route: Router) {
+    this.email = new FormControl('');
+    this.name = new FormControl('');
+    this.password = new FormControl('');
+   }
 
   ngOnInit(): void {
   }
 
+  submitForm(){
+    this.user.nombre = this.name.value;
+    this.user.correo = this.email.value;
+    this.user.contraseña = this.password.value;
+    console.log(this.user)
+    this.userService.createUser(this.user)
+      .subscribe((res) => {
+        if(res){
+          this.route.navigate(['/catalogo']);
+        }
+      })
+  }
 }
